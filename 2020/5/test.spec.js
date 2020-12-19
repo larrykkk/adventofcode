75de405437d76ad2547435ba87f9fa39de05d436
larry <larry@blueplanet.com.tw>
2020-12-19 16:24:01 +0800

    5-2

--- 2020/5/test.spec.js
+++ 2020/5/test.spec.js
@@ -1,4 +1,5 @@
 const one = require("./one");
+const two = require("./two");
 
 describe("Part One", () => {
   it("should be pass", () => {
@@ -8,3 +9,12 @@ describe("Part One", () => {
     expect(one(input)).toBe(820);
   });
 });
+
+// describe("Part Two", () => {
+//   it("should be pass", () => {
+//     let input = `BFFFBBFRRR
+//     FFFBBBFRRR
+//     BBFFBBFRLL`;
+//     expect(one(input)).toBe(820);
+//   });
+// });
